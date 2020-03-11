3e8724eaa230629fbc3717f72fa5aec6e4e5c5eb
weiqiangbing <weiqiangbing@moqing.com>
2020-03-11 20:34:59 +0800

    提交代码

--- src/views/rechargeActivities.vue
+++ src/views/rechargeActivities.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="rechargeActivities">
-    <div class="rechar_header">
+    <div class="rechar_banner">
 
     </div>
     <div class="rechar_box">
@@ -17,30 +17,7 @@
           <li>礼品</li>
         </ul>
       </div>
-      <div class="rechar_item">
-        <div class="rechar_title">
-          <div class="title_word">充值的项目</div>
-          <div class="rechar_btn">充值</div>
-        </div>
-        <ul class="rechar_content">
-          <li>礼品</li>
-          <li>礼品</li>
-          <li>礼品</li>
-          <li>礼品</li>
-        </ul>
-      </div>
-      <div class="rechar_item">
-        <div class="rechar_title">
-          <div class="title_word">充值的项目</div>
-          <div class="rechar_btn">充值</div>
-        </div>
-        <ul class="rechar_content">
-          <li>礼品</li>
-          <li>礼品</li>
-          <li>礼品</li>
-          <li>礼品</li>
-        </ul>
-      </div>
+    
     </div>
   </div>
 </template>
@@ -70,17 +47,25 @@ export default {
   .rechargeActivities{
     width: 100%;
     height: 100%;
+    .rechar_banner{
+      width: 100%;
+      height: 229px;
+      background-color: #ff874b;
+    }
     // background-color: @warningColor; 
     .rechar_box{
       width: 100%;
       padding: 20px 30px;
       box-sizing: border-box;
+      background-color: @bgColor;
 
       .rechar_item{
         border: 1px solid #ccc;
-        height: 230px;
+        width: 329px;
+        height: 138px;
         padding-top: 20px;
         margin-top: 40px;
+
         .rechar_title{
           padding: 0 7px;
           border-bottom: 1px solid #ddd;
@@ -102,13 +87,14 @@ export default {
         }
         .rechar_content{
           display: flex;
-          padding: 20px;
+          // width: 42px;
+          height: 80px;
           justify-content: space-around;
-
           li{
             background-color: rosybrown;
-            width: 120px;
-            height: 120px;
+            width: 50px;
+            height: 80px;
+            font-size: 16px;
           }
         }
       }
